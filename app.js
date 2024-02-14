e6b294c8f8392826f403c9c328b53059ace1aef3
baetenJ <jbaeten@ivytech.edu>
2024-02-13 20:04:14 -0500

    Using EJS Server Side Rendering

--- app.js
+++ app.js
@@ -4,27 +4,33 @@ const express = require('express');
 
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.write();
-    // res.end();
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+       {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+       {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+       {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs});
 });
+
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About'});
 });
-// redirection
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create A New Blog'});
+})
 
 // 404 page with middleware function
 // must be at the bottom
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404'});
 });
